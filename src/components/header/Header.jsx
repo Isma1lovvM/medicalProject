c7a42d3517b0f2e33edec923cdd0c006d7b15d4c
Isma1lovvM <isma1lovv.m.h.n@gmail.com>
2025-12-10 18:21:33 +0500

    task done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -16,41 +16,7 @@ function Header() {
     <>
       <header className={scrolled ? "scrolled" : ""}>
         <div className="container">
-          <nav className="navbar">
-            <NavLink className={"isActive"} to={"/"}>
-              <h1 className="logo_text">
-                E - <span className="nav_logo_span">sheba</span>
-              </h1>
-            </NavLink>
-            <ul className="nav_list">
-              <li className="nav_item">
-                <NavLink
-                  className={({ isActive }) =>
-                    isActive ? "active" : "nav_item"
-                  }
-                  to={"/"}
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li className="nav_item">
-                <NavLink
-                  className={({ isActive }) =>
-                    isActive ? "active" : "nav_item"
-                  }
-                  to={"/about"}
-                >
-                  About
-                </NavLink>
-              </li>
-              <li className="nav_item">Application</li>
-              <li className="nav_item">History</li>
-            </ul>
-            <ul className="nav_btns_list">
-              <button className="login_btn">Log in</button>
-              <button className="signup_btn">Sign up</button>
-            </ul>
-          </nav>
+          
         </div>
       </header>
     </>
