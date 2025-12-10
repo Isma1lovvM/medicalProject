fa5980c544bd5c728a1ce5d6129086024e539635
Isma1lovvM <isma1lovv.m.h.n@gmail.com>
2025-12-10 17:47:00 +0500

    xato

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import Consulting from "./components/consulting/Consulting";
 import ChooseUs from "./components/chooseus/ChooseUs";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <Consulting />
-    <ChooseUs />
+      <Consulting />
+      <ChooseUs />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
