f50ce6f5c19708db7fc2b26282e3f6acf7d62c28
 <ahmedov13nurmuhammad@gmail.com>
2025-12-10 18:04:13 +0500

    nurmuhammad

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import Consulting from "./components/consulting/Consulting";
-import ChooseUs from "./components/chooseus/ChooseUs";
-
-import './App.css'
+import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
-
   return (
     <>
-    <Consulting />
-    <ChooseUs />
+      <Header />
+      <h1>Salom</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
