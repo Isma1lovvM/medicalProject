e589ce9009ba142959404eadc5ee1a2670bb29de
Isma1lovvM <isma1lovv.m.h.n@gmail.com>
2025-12-15 17:29:43 +0500

    project ozgartirildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    AOS.init({ duration: 800, once: true });
+  }, []);
+
   return (
     <>
       <Header />
--- src/components/consulting/Consulting.jsx
+++ src/components/consulting/Consulting.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import './consulting.css'
-import rasm1 from '../../assets/Vector.png'
-import rasm2 from '../../assets/Vector (1).png'
-import rasm3 from '../../assets/Vector (3).png'
-import rasm4 from '../../assets/Vector (2).png'
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./consulting.css";
+import rasm1 from "../../assets/Vector.png";
+import rasm2 from "../../assets/Vector (1).png";
+import rasm3 from "../../assets/Vector (3).png";
+import rasm4 from "../../assets/Vector (2).png";
 
 function Consulting() {
+  useEffect(() => {
+    AOS.init({ duration: 800, once: true });
+  }, []);
+
   const data = [
     {
       title: "Covid-19 Test",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sem velit viverra amet faucibus.",
-      icon: rasm1,      
+      icon: rasm1,
     },
     {
       title: "Heart Lungs",
@@ -29,25 +35,39 @@ function Consulting() {
     },
   ];
 
+  const animations = [
+    "fade-down-right",
+    "fade-up-right",
+    "fade-up-left",
+    "fade-down-left",
+  ];
+
   return (
     <div className="container">
-      <h2 className='ws'>Our Consulting Specialists</h2>
-      
-      <div className='consulting_div'>
+      <h2 className="ws">Our Consulting Specialists</h2>
+
+      <div className="consulting_div">
         <div className="consulting-wrapper">
-          {data.map((item, index) => (
-            <div key={index} className="consulting-card">
-              <div className="icon">
-                <img src={item.icon} alt={item.title} />
+          {data.map((item, index) => {
+            const chosenAnimation = animations[index % animations.length];
+            return (
+              <div
+                key={index}
+                data-aos={chosenAnimation}
+                className="consulting-card"
+              >
+                <div className="icon">
+                  <img src={item.icon} alt={item.title} />
+                </div>
+                <h3>{item.title}</h3>
+                <p>{item.desc}</p>
               </div>
-              <h3>{item.title}</h3>
-              <p>{item.desc}</p>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Consulting;
